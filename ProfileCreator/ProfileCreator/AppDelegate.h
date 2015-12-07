ed8716bd946eb21f038c5488a4c056f1cb9fb5bb
Erik Berglund <erikberglund@Demo.local>
2015-12-07 17:44:37 +0100

    Initial Commit

---
+++ ProfileCreator/ProfileCreator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProfileCreator
+//
+//  Created by Erik Berglund on 2015-12-07.
+//  Copyright © 2015 Erik Berglund. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ProfileCreator/ProfileCreator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ProfileCreator
+//
+//  Created by Erik Berglund on 2015-12-07.
+//  Copyright © 2015 Erik Berglund. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
